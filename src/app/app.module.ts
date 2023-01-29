8259aa22ebbd03e912c2a92ed29b31005f0564d0
Ederson Rodrigues <ederson.rodrigues777@gmail.com>
2023-01-29 15:28:01 -0300

    transaction-form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TransactionFormComponent } from './transaction-form/transaction-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransactionFormComponent
   ],
   imports: [
     BrowserModule
